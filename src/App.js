e35f7aee46ce90d4d6f4927849b3251b50fac2a5
ChenWei <chen158_7@126.com>
2023-12-15 16:37:31 +0800

    hahahah

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const repo = "https://chenwei0922.github.io/PWA-Demo/";
+let deferredPrompt;  
+    
 function App() {
+  const [installable, setInstallable] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      // Update UI notify the user they can install the PWA
+      setInstallable(true);
+    });
+
+    window.addEventListener('appinstalled', () => {
+      // Log install to analytics
+      console.log('INSTALL: Success');
+    });
+  }, []);
+
+  const handleInstallClick = (e) => {
+      // Hide the app provided install promotion
+      setInstallable(false);
+      // Show the install prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the install prompt');
+        } else {
+          console.log('User dismissed the install prompt');
+        }
+      });
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Install Demo</h2>
+        {installable &&
+          <button className="install-button" onClick={handleInstallClick}>
+            INSTALL ME
+          </button>
+        }
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <a href={repo} className="App-link">View source on GitHub</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
